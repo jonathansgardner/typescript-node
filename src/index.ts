0dc1b514562d19b6df622953acc54978f40c5222
Jonathan Gardner <jonathangardner@Cafofinghams-iMac.local>
2021-06-30 20:33:58 -0400

    adds eslint rules

--- src/index.ts
+++ src/index.ts
@@ -1,115 +1,132 @@
 // consecutive variables - global variables must be capitalized
-const A = 2,
-			B = 1,
-			C = 3,
-			D = 5;
+const a = 2;
+const b = 1;
+const c = 3;
+const d = 5;
 let v = 5;
 
 v += 3;
 v **= 3;
 
+function func<T = number>( arg: T ): T {
+  return arg;
+}
+
+func( 5 );
+
 // interface keyword
 interface T1 {
   greeting: string;
-	printMessage: () => string;
+  printMessage: () => string;
 }
 
-console.log( A, B, C, D );
+console.log( a, b, c, d );
 console.log( v );
 
 {
-	interface Obj {
-		'1': string;
-		test: string;
-	};
+  interface Obj {
+    '1': string;
+    test: string;
+  };
 
-	const obj: Obj = {
-		'1': 'one',
-		test: 'testing'
-	};
+  const obj: Obj = {
+    '1': 'one',
+    test: 'testing'
+  };
 
-	console.log( obj );
+  console.log( obj );
 }
 
-function* other() : Generator<number> {
-	yield 2;
+function* other(): Generator<number> {
+  yield 2;
 }
 
-function* generator() : Generator<number> {
-	yield* other();
+function* generator(): Generator<number> {
+  yield* other();
 }
 
 generator();
 
 // iife
 ( function( arg1: string, arg2: number, arg3: boolean ): void {
-	console.log( arg1, arg2, arg3 );
+  console.log( arg1, arg2, arg3 );
 
-	// function declaration
-	function printMessage(
-		message: string
-	) : ( string | null ) {
-		return message
-			? message + 1
-			: message.length > 2
-				? message + 2
-				: null;
-	}
+  // function declaration
+  function printMessage(
+    num: number
+  ): ( number | null ) {
+    return num
+      ? num + 1
+      : num > 2
+        ? num + 2
+        : null;
+  }
 
-	// consecutive variables
-	let y = 15;
-	const x = ( y++, arg1 );
+  // consecutive variables
+  let y = 15;
+  const x = ( y++, arg2 );
 
-	// expression
-	printMessage( x );
+  // expression
+  printMessage( x );
 } )( 'hello', 2, true );
 
 // consecutive variables
-const ARR = [
-				1,
-				2,
-				3
-			],
-			TEST_BOOL = ( ARR[ 0 ] + ARR[ 1 ] ) * ARR[ 2 ];
+const ARR: number[] = [
+  1,
+  2,
+  3
+];
+const TEST_BOOL = ( ARR[ 0 ] + ARR[ 1 ] ) * ARR[ 2 ];
 
 console.log( TEST_BOOL );
 
+interface Accumulator { readonly [key: string]: number };
+
+const reducedArr = ARR.reduce<Accumulator>(
+  ( acc: Accumulator, num: number ) => (
+    { ...acc, [ num ]: num }
+  ),
+  {}
+);
+
+console.log( reducedArr );
+
 ARR.reverse()
-	.sort();
+  .sort();
 
 class TestClass implements T1 {
-	public static readonly TEST_PROPERTY: string = 'TEST';
-	public greeting = 'hello';
+  public static readonly TEST_PROPERTY: string = 'TEST';
+  public greeting = 'hello';
 
-	get greet(): string {
-		return `${ this.greeting } There!`;
-	}
+  get greet(): string {
+    return `${ this.greeting } There!`;
+  }
 
-	set greet( value: string ) {
-		this.greeting = value;
-	}
+  set greet( value: string ) {
+    this.greeting = value;
+  }
 
-	constructor() {}
+  constructor() {}
 
-	private getPrintMessage(): string {
-		return `${ this.greeting } I'm TestClass`;
-	}
+  private getPrintMessage(): string {
+    return `${ this.greeting } I'm TestClass`;
+  }
 
-	public printMessage(): string {
-		return this.getPrintMessage();
-	}
+  public printMessage(): string {
+    return this.getPrintMessage();
+  }
 }
 
 new TestClass().printMessage();
 
 switch ( v ) {
-	case 1:
-		console.log( 'hi' );
+  case 1:
+    console.log( 'hi' );
 
-		break;
+    break;
 
-	default:
-		break;
+  default:
+    break;
 }
 
 export default TestClass;
